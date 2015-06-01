84b3297868afb8acf694f3015da20ece421ed4ef
clonq <ovi.comes@gmail.com>
2015-05-31 22:04:10 -0400

    explicit defaults

--- component.js
+++ component.js
@@ -3,9 +3,17 @@ module.exports = function(){
     this.init = function(config) {
         var that = this;
         this.params = _.defaults(config||{}, defaults)
+        process.on('user:register', function(payload){
+        	console.log('clonq/revo-user: user:register: ', payload)
+        })
     }
 }
 
 var defaults = module.exports.defaults = {
-    models: [user]
+	listen: 'user:register',
+    models: {
+    	user: {
+    		supportedMethods: ['register']
+    	}
+    }
 }
