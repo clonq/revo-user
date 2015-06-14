8623daff6172c3450752f88c21600cd93e8a5172
clonq <ovi.comes@gmail.com>
2015-06-14 19:25:14 -0400

    invite code handling, skeleton impl for user find/update

--- component.js
+++ component.js
@@ -5,27 +5,48 @@ module.exports = function(){
     var dao = require('daoi');
 
     this.init = function(config) {
-        var that = this;
+        var self = this;
+        var daoImpl = dao.use(dao.MEMORY);
         this.params = _.defaults(config||{}, defaults)
+        process.on('user:update', function(user){
+console.log('user:update', user);
+        });
+        process.on('user:find', function(criteria){
+console.log('user:find', criteria);
+            // fake search result
+            process.emit('user:find.by.email.'+criteria.email+'.response', {fake: 'user'});//todo
+            // daoImpl.user.find(pin)
+            // .then(function(user){
+            // })
+            // .catch(function(err){
+            // })
+        });
         process.on('user:register', function(pin){
             console.log('clonq/revo-user: user:register: ', pin);
             var pout = {};
             var errors = validate(pin);
             if(errors.length == 0) {
-                var impl = dao.use(dao.MEMORY);
                 dao.register('user');
-                impl.user.create(pin)
+                daoImpl.user.create(pin)
                 .then(function(user){
-                    pout = {
-                        user: user,
-                        success: {
-                            message: "user has been registered successfully"
-                        }
+                    if(!!user.inviteCode) {
+                        var invitationPayload = {
+                            code: user.inviteCode,
+                            email: user.email
+                        };
+                        process.emit('invitation:process', invitationPayload);
                     }
-                    process.emit('user:register.response', pout);
+                    // pout = {
+                    //     user: user,
+                    //     success: {
+                    //         message: "user has been registered successfully"
+                    //     }
+                    // }
+                    // process.emit('user:register.response', pout);
                 })
                 .catch(function(err){
-                    done(err);
+                    pout.error = { message: err.message };
+                    process.emit('user:register.response', pout);
                 })
             } else {
                 pout.error = { message: errors[0].message };
@@ -51,7 +72,7 @@ var defaults = module.exports.defaults = {
     listen: 'user:register',
     models: {
         user: {
-            supportedMethods: ['register']
+            supportedMethods: ['register', 'find']
         }
     }
 }
